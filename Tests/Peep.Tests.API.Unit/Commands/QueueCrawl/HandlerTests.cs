1e3471b34cdd25997efbbd4f7b3b753d2b69a631
Joe Thompson <joseph.thompson>
2021-02-08 22:31:03 +0000

    added failing tests for handlers and validation

---
+++ Tests/Peep.Tests.API.Unit/Commands/QueueCrawl/HandlerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peep.Tests.API.Unit.Commands.QueueCrawl
+{
+    [TestClass]
+    [TestCategory("API - Unit - Queue Crawl Handler")]
+    public class HandlerTests
+    {
+        [TestMethod]
+        public async Task Does_Something()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Tests/Peep.Tests.API.Unit/Queries/GetCrawl/HandlerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peep.Tests.API.Unit.Queries.GetCrawl
+{
+    [TestClass]
+    [TestCategory("API - Unit - Get Crawl Handler")]
+    public class HandlerTests
+    {
+        [TestMethod]
+        public async Task Does_Something()
+        {
+            Assert.Fail();
+        }
+    }
+}
