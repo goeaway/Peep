2fa3333b0bec0de9494fb4c72f3a6c3742eda765
Joe Thompson <joseph.thompson>
2021-02-26 20:41:49 +0000

    added network back to docker compose

--- API/Peep.API/Controllers/CrawlController.cs
+++ API/Peep.API/Controllers/CrawlController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Peep.API.Application.Requests.Commands.CancelCrawl;
 using Peep.API.Application.Requests.Commands.QueueCrawl;
 using Peep.API.Application.Requests.Queries.GetCrawl;
 using Peep.API.Models.DTOs;
-using Peep.Core;
 
 namespace Peep.API.Controllers
 {
